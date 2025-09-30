3a7aaccdb9ca2b21c5adc6ef22fc9df0ed78cc01
Youssef-Shehata <31982488+Youssef-Shehata@users.noreply.github.com>
2025-09-30 06:59:28 +0300

    we ball

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Head from 'next/head';
-import Image from 'next/image';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Head from "next/head";
+import Image from "next/image";
 
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Youssef-Shehata',
-  description: 'Software Engineer Portfolio',
+  title: "Youssef-Shehata",
+  description: "Software Engineer Portfolio",
 };
 
 export default function RootLayout({
@@ -19,10 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     <Head>
-            <title>Youssef Shehata</title>
-            <Image rel="icon" src="/favicon.ico" alt=''/>
-        </Head>
+      <Head>
+        <title>Youssef Shehata</title>
+        <Image rel="icon" src="/favicon.ico" alt="" />
+      </Head>
 
       <body className={inter.className}>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,138 +1,134 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { Card, CardFooter, CardDescription, CardHeader, CardTitle, CardContent } from '@/app/components/ui/card';
-import { Avatar, AvatarFallback, AvatarImage } from '@/app/components/ui/avatar';
-import { Github, Linkedin, Twitter } from 'lucide-react';
-import projectsData from '@/app/data/projects.json'
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Card,
+  CardFooter,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from "@/app/components/ui/card";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/app/components/ui/avatar";
+import { Github, Linkedin, Twitter } from "lucide-react";
+import projectsData from "@/app/data/projects.json";
 export default function Home() {
-    return (
-        <main className=" min-h-screen bg-[#f5f5f5] py-12 px-4 sm:px-6 lg:px-8">
+  return (
+    <main className=" min-h-screen bg-[#f5f5f5] py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto">
+        <div className="text-center mb-16">
+          <Avatar className="h-32 w-32 mx-auto mb-4">
+            <AvatarImage
+              src="https://github.com/Youssef-Shehata.png"
+              alt="avatar"
+            />
+            <AvatarFallback>YS</AvatarFallback>
+          </Avatar>
 
-            <div className="max-w-5xl mx-auto">
+          <h1 className="text-4xl font-bold text-gray-900 mb-4">
+            {projectsData.info.name}
+          </h1>
 
-                <div className="text-center mb-16">
-                    <Avatar className="h-32 w-32 mx-auto mb-4">
-                        <AvatarImage src="https://github.com/Youssef-Shehata.png" alt="avatar" />
-                        <AvatarFallback>YS</AvatarFallback>
-                    </Avatar>
+          <p className="text-xl text-gray-600 mb-6">
+            {projectsData.info.title}
+          </p>
+          <div className="flex justify-center space-x-4">
+            {projectsData.info.links.Github && (
+              <a
+                href={projectsData.info.links.Github.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <Github className="h-6 w-6" />
+              </a>
+            )}
+            {projectsData.info.links.LinkedIn && (
+              <a
+                href={projectsData.info.links.LinkedIn.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <Linkedin className="h-6 w-6" />
+              </a>
+            )}
 
-                    <h1 className="text-4xl font-bold text-gray-900 mb-4">Youssef Shehata</h1>
+            {projectsData.info.links.Twitter && (
+              <a
+                href={projectsData.info.links.Twitter.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-900"
+              >
+                <Twitter className="h-6 w-6" />
+              </a>
+            )}
+          </div>
+        </div>
 
-                    <p className="text-xl text-gray-600 mb-6">
-                        Software Engineer
-                    </p>
-                    <div className="flex justify-center space-x-4">
-                        <a
-                            href="https://github.com/Youssef-Shehata"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-600 hover:text-gray-900"
-                        >
-                            <Github className="h-6 w-6" />
-                        </a>
-                        <a
-                            href="https://linkedin.com/in/youssef-shehata-shaaban"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-600 hover:text-gray-900"
-                        >
-                            <Linkedin className="h-6 w-6" />
-                        </a>
+        <div className="prose max-w-none mb-16">
+          <p className="text-gray-900 text-xl px-12 ">
+            {projectsData.info.summary.map((s) => {
+              return <div>{s}</div>;
+            })}
+          </p>
+        </div>
 
+        {/* Projects */}
+        <div key="" className="grid grid-rows  gap-4">
+          {projectsData.projects.map((project) => (
+            <Card
+              key={project.id}
+              className="flex-col bg-[#f1f5f9] content-between align-items:space-between h-full hover:shadow-lg transition-shadow  content-between"
+            >
+              <Link
+                key={project.id}
+                target="_blank"
+                href={project.link || project.github}
+              >
+                <CardHeader className="gap-2 ">
+                  <CardTitle>{project.title}</CardTitle>
+                  <CardDescription>{project.summary}</CardDescription>
+                </CardHeader>
+              </Link>
+              <CardContent />
 
-                        <a href="https://x.com/seijuro_tj"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-600 hover:text-gray-900"
-                        >
-                            <Twitter className='h-6 w-6' />
-                        </a>
-
-
-
-
-
-                        <a
-                            href="https://leetcode.com/youssefshehata58/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                alt="leetcode icon"
-                                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg"
-                                width={18} height={18}
-                            />
-                        </a>
-
+              <CardFooter className="flex justify-between align-items:center ">
+                <div className="flex w-max align-items:center justify-start flex-wrap gap-2">
+                  {project.live && (
+                    <div className="flex align-items:center justify-center bg-[#0088c1] text-white py-1 px-3 rounded border-solid border">
+                      Live
                     </div>
+                  )}
+                  {project.stack.map((a) => (
+                    <div
+                      key={a}
+                      className="flex align-items:center  justify-center bg-[#003134] text-white py-1 px-3 rounded border-solid border"
+                    >
+                      {a}
+                    </div>
+                  ))}
                 </div>
-
-                <div className="prose max-w-none mb-16">
-                    <p className="text-lg px-12 text-gray-600">
-                        Hi , im youssef shehata, a computer science grad in love with hard problems and good programming tools,
-                        occasionaly creating both. a fan of meta programming and ricing my dev environment.
-                        <br />
-                        <br />
-                        currently learning more about databases and the beauriful engineering the goes behind them.
-
-                        <br />
-                        in my free time i like to draw (will be posting some of my drawings soon)
-                        <br />
-                        <br />
-                        Here is some of my recent work :
-                    </p>
-                </div>
-
-                {/* Projects */}
-                <div key="" className="grid grid-rows  gap-4">
-                    {projectsData.projects.map((project) => (
-
-                        <Card key={project.id} className="flex-col bg-[#f1f5f9] content-between align-items:space-between h-full hover:shadow-lg transition-shadow  content-between">
-                            <Link key={project.id} target="_blank" href={project.link || project.github}>
-
-                                <CardHeader className='gap-2 '>
-
-                                    <CardTitle>{project.title}</CardTitle>
-                                    <CardDescription >
-                                        {project.summary}
-
-                                    </CardDescription>
-                                </CardHeader>
-                            </Link>
-                            <CardContent />
-
-
-                            <CardFooter className='flex justify-between align-items:center '>
-                                <div className='flex w-max align-items:center justify-start flex-wrap gap-2'>
-                                    {project.live &&
-                                        <div className="flex align-items:center justify-center bg-[#0088c1] text-white py-1 px-3 rounded border-solid border">
-                                            Live
-
-                                        </div>
-                                    }
-                                    {project.stack.map(a => (
-                                        <div key={a} className="flex align-items:center  justify-center bg-[#003134] text-white py-1 px-3 rounded border-solid border">
-                                            {a}
-                                        </div>
-                                    ))}
-
-                                </div>
-                                <a
-                                    href={project.github}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="w-full text-sm px-3 p-1 w-max text-gray-500 hover:text-gray-700 text-right "
-                                    onClick={(e) => e.stopPropagation()}
-                                >
-                                    View on GitHub →
-                                </a>
-                            </CardFooter>
-                        </Card>
-                    ))}
-                </div>
-            </div>
-        </main >
-    );
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-full text-sm px-3 p-1 w-max text-gray-500 hover:text-gray-700 text-right "
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  View on GitHub →
+                </a>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
